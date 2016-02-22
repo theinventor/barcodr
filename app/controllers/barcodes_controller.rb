7b9c539f7ac75fdb88c3c02ee55b4b5ca4877726
Troy Anderson <the.inventor@gmail.com>
2016-02-21 20:34:49 -0800

    allow iframing

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -1,4 +1,6 @@
 class BarcodesController < ApplicationController
+  before_filter :allow_iframe_requests
+
   def index
   end
 
--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,4 +1,6 @@
 class LabelsController < ApplicationController
+  before_filter :allow_iframe_requests
+
   def index
   end
 
